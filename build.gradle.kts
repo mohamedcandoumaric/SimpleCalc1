7855cc6eab5cc7cae63e5d1f61dc0f1109bd5de5
mohamedcandoumaric <fayazmohideen400@gmail.com>
2026-05-26 09:20:02 +0530

    push11

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,8 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly ("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.test {
