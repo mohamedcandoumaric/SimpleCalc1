a30c68b15a727ee80cfc45ba5135da7a73c19f17
mohamedcandoumaric <fayazmohideen400@gmail.com>
2026-05-26 09:12:55 +0530

    pushchanges

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     java
-    application
-}
+ }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
@@ -12,10 +11,7 @@ repositories {
 
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-}
-
-application {
-    mainClass.set("org.example.Main")
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
